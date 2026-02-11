c90dc9db55b90d6e9e5f60c15b96701c50a41740
olowo-elisha-ibukun <elishaibukunolowo@gmail.com>
2026-02-11 19:26:11 +0100

    olowo

--- project/scripts/about.js
+++
@@ -1,6 +0,0 @@
-// about.js - About page setup
-import { initPage } from './modules/setupPage.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initPage();
-});
--- project/scripts/index.js
+++
@@ -1,6 +0,0 @@
-// index.js - Home page setup
-import { initPage } from './modules/setupPage.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  initPage();
-});
